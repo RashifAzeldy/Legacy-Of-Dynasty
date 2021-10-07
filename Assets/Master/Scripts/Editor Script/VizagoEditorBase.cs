f06ad8c87d14511b484177d38a5148bdf2782231
Kuroake Vizago <zaebiyt@gmail.com>
2021-10-07 13:54:38 +0700

    Base Editor Added

---
+++ Assets/Master/Scripts/Editor Script/VizagoEditorBase.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace VEditor
+{
+    [CustomEditor(typeof(MonoBehaviour), true)]
+    [CanEditMultipleObjects]
+    public class VizagoEditorBase : Editor
+    {
+        Dictionary<string, SerializedProperty> serializeProps = new Dictionary<string, SerializedProperty>();
+
+        protected void OnEnable()
+        {
+            
+        }
+        
+        public sealed override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUI.indentLevel = 0;
+
+            OnInspector();
+            
+            serializedObject.ApplyModifiedProperties();
+
+        }
+
+        /// <summary>
+        /// Edit inspector properties here, all change already applied automatically
+        /// </summary>
+        protected virtual void OnInspector() 
+        { 
+        
+        }
+
+        protected SerializedProperty FindProperty(string name)
+        {
+            SerializedProperty res;
+
+            if (!serializeProps.TryGetValue(name, out res))
+                res = serializeProps[name] = serializedObject.FindProperty(name);
+            if (res == null) 
+                throw new System.ArgumentException("Can't Find : " + name);
+
+            return res;
+        }
+    }
+}
+
