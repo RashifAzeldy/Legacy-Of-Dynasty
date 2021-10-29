44a5351c35dd85b6c4877f55c8b4e0c5657b2509
Kuroake Vizago <zaebiyt@gmail.com>
2021-10-29 17:26:59 +0700

    Object Pooling System Added

---
+++ Assets/Master/Scripts/GameManager/ObjectPoolerManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolerManager : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class PoolObject
+    {
+
+        public string TagId;
+        public GameObject Prefab;
+        public int Amount;
+
+    }
+
+    #region Singleton
+
+    private static ObjectPoolerManager _instance;
+    private ObjectPoolerManager()
+    {
+        _instance = this;
+    }
+
+    public static ObjectPoolerManager Instance { get => _instance; }
+
+
+    #endregion
+
+    public List<PoolObject> poolObjects = new List<PoolObject>();
+
+    public Dictionary<string, Queue<GameObject>> poolsDictionary = new Dictionary<string, Queue<GameObject>>();
+
+    public void InitializePoolObject(string poolObjectTag)
+    {
+        foreach (PoolObject pool in poolObjects.ToArray())
+        {
+            if (pool.TagId == poolObjectTag)
+            {
+
+                Queue<GameObject> objectPool = new Queue<GameObject>();
+
+                for (int i = 0; i < pool.Amount; i++)
+                {
+                    GameObject obj = Instantiate(pool.Prefab);
+                    obj.SetActive(false);
+                    objectPool.Enqueue(obj);
+                }
+
+                poolsDictionary.Add(pool.TagId, objectPool);
+
+                break;
+
+            }
+            else
+                throw new System.ArgumentException("Can't pool object with tag :" + poolObjectTag);
+
+        }
+    }
+
+    public GameObject SpawnObjectFromPool(string poolObjectsTag, Vector3 position, Quaternion rotation)
+    {
+
+        Debug.Log(poolObjects.Count);
+
+        if (!poolsDictionary.ContainsKey(poolObjectsTag))
+            InitializePoolObject(poolObjectsTag);
+        
+        GameObject cacheObjToSpawn = poolsDictionary[poolObjectsTag].Dequeue();
+
+        cacheObjToSpawn.SetActive(true);
+        cacheObjToSpawn.transform.position = position;
+        cacheObjToSpawn.transform.rotation = rotation;
+
+        poolsDictionary[poolObjectsTag].Enqueue(cacheObjToSpawn);
+
+        return cacheObjToSpawn;
+
+    }
+
+    public void DestroyPoolObjectFromScene(string poolObjectsTag, GameObject gameObject)
+    {
+        if (poolsDictionary.ContainsKey(poolObjectsTag))
+            if (poolsDictionary[poolObjectsTag].Contains(gameObject))
+                gameObject.SetActive(false);    
+    }
+
+}
